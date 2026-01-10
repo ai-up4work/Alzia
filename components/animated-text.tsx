080c9d27a1ce7b410bc0ccf6b0c3fa36524f529d
SafnasKaldeen <safnaskm.20@uom.lk>
2026-01-10 16:52:39 +0530

    font size

--- components/animated-text.tsx
+++ components/animated-text.tsx
@@ -9,7 +9,7 @@ interface AnimatedTextProps {
 
 export function AnimatedText({ text, delay = 0 }: AnimatedTextProps) {
   return (
-    <span className="text-7xl">
+    <span className="text-8xl">
       {text.split("").map((char, index) => (
         <motion.span
           key={index}
